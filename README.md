b0ad159b8c0f57440509c0bec15688b7edbf9bd1
superzoeyian <ranyueranyue@outlook.com>
2022-10-11 16:22:48 +0800

    解析器更新至DIF-universal-resolver -latest版本

--- README.md
+++ README.md
@@ -1,29 +1,29 @@
 ![DIF Logo](https://raw.githubusercontent.com/decentralized-identity/universal-resolver/master/docs/logo-dif.png)
 
-# Universal Resolver Driver: did:btcr
+# Universal Resolver Driver: did:iid
 
-This is a [Universal Resolver](https://github.com/decentralized-identity/universal-resolver/) driver for **did:btcr** identifiers.
+This is a [Universal Resolver](https://github.com/decentralized-identity/universal-resolver/) driver for **did:iid** identifiers.
 
 ## Specifications
 
 * [Decentralized Identifiers](https://w3c.github.io/did-core/)
-* [DID Method Specification](https://w3c-ccg.github.io/didm-btcr/)
+* [DID Method Specification](https://github.com/InspurIndustrialInternet/iid/blob/main/doc/en/InspurChain_DID_protocol_Specification.md)
 
 
 ## Example DIDs
 
 ```
- did:btcr:xz35-jznz-q9yu-ply
- did:btcr:xkrn-xz7q-qsye-28p
- did:btcr:x705-jznz-q3nl-srs
+did:iid:3QUs61mk7a9CdCpckriQbA5emw8pubj6RMtHXP6gD66YbcungS6w2sa
+did:iid:3k4KHKzF6FTiEpuBiCTMUYfzhDQyqZoye9196ECnYhcNvJyf4WjCQpP
+
 ```
 
 ## Build and Run (Docker)
 
 ```
-docker build -f ./docker/Dockerfile . -t universalresolver/driver-did-btcr
-docker run -p 8080:8080 universalresolver/driver-did-btcr
-curl -X GET http://localhost:8080/1.0/identifiers/did:btcr:xz35-jznz-q9yu-ply
+docker build -f ./docker/Dockerfile . -t universalresolver/driver-did-iid
+docker run -p 8080:8080 universalresolver/driver-did-iid
+curl -X GET http://localhost:8080/1.0/identifiers/did:iid:xz35-jznz-q9yu-ply
 ```
 
 ## Build (native Java)
@@ -34,41 +34,10 @@ curl -X GET http://localhost:8080/1.0/identifiers/did:btcr:xz35-jznz-q9yu-ply
 
 The driver recognizes the following environment variables:
 
-### `uniresolver_driver_did_btcr_bitcoinConnection`
-
- * Specifies how the driver interacts with the Bitcoin blockchain.
- * Possible values: 
-   * `bitcoind`: Connects to a [bitcoind](https://bitcoin.org/en/full-node) instance via JSON-RPC
-   * `btcd`: Connects to a [btcd](https://github.com/btcsuite/btcd) instance via JSON-RPC
-   * `bitcoinj`: Connects to Bitcoin using a local [bitcoinj](https://bitcoinj.github.io/) client
-   * `blockcypherapi`: Connects to [BlockCypher's API](https://www.blockcypher.com/dev/bitcoin/)
- * Default value: `blockcypherapi`
-
-### `uniresolver_driver_did_btcr_rpcUrlMainnet`
-
- * Specifies the JSON-RPC URL of a bitcoind/btcd instance running on Mainnet.
- * Default value: `http://user:pass@localhost:8332/`
-
-### `uniresolver_driver_did_btcr_rpcUrlTestnet`
-
- * Specifies the JSON-RPC URL of a bitcoind/btcd instance running on Testnet.
- * Default value: `http://user:pass@localhost:18332/`
-
-### `uniresolver_driver_did_btcr_rpcCertMainnet`
-
- * Specifies the server TLS certificate of the bitcoind/btcd instance running on Mainnet.
- * Default value: ``
-
-### `uniresolver_driver_did_btcr_rpcCertTestnet`
-
- * Specifies the server TLS certificate of the bitcoind/btcd instance running on Testnet.
- * Default value: ``
-
 ## Driver Metadata
 
 The driver returns the following metadata in addition to a DID document:
-
-* `fragmentUri`: ...
-* `chain`: ...
-* `blockHeight`: ...
-* `blockIndex`: ...
+* `version`: The DID version.
+* `proof`: Some proof info about the DID document.
+* `created`: The DID create time.
+* `updated`: The DID document last update time.
--- src/main/java/uniresolver/driver/did/iid/DidIidDriver.java
+++ src/main/java/uniresolver/driver/did/iid/DidIidDriver.java
@@ -1,9 +1,9 @@
 package uniresolver.driver.did.iid;
 
-import did.Authentication;
-import did.DIDDocument;
-import did.PublicKey;
-import did.Service;
+import foundation.identity.did.DID;
+import foundation.identity.did.DIDDocument;
+import foundation.identity.did.VerificationMethod;
+import foundation.identity.did.parser.ParserException;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -18,12 +18,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uniresolver.ResolutionException;
 import uniresolver.driver.Driver;
-import uniresolver.result.ResolveResult;
+import uniresolver.result.ResolveDataModelResult;
 
 import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import foundation.identity.did.Service;
+import uniresolver.driver.did.iid.Util.Base64;
 
 
 public class DidIidDriver implements Driver {
@@ -36,7 +43,7 @@ public class DidIidDriver implements Driver {
 
     public static final String[] DIDDOCUMENT_AUTHENTICATION_TYPES = new String[]{"Secp256k1"};
 
-    public static final String DEFAULT_IID_URL = "http://10.221.19.217";
+    public static final String DEFAULT_IID_URL = "http://117.73.2.209:9999";
 
     public static final HttpClient DEFAULT_HTTP_CLIENT = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy()).build();
 
@@ -44,18 +51,26 @@ public class DidIidDriver implements Driver {
 
     private HttpClient httpClient = DEFAULT_HTTP_CLIENT;
 
+    public static void main(String[] args) throws ResolutionException, IllegalArgumentException, ParserException {
+        DID did = DID.fromString("did:iid:3QUs61mk7a9CdCpckriQbA5emw8pubj6RMtHXP6gD66YbcungS6w2sa");
+        ResolveDataModelResult rdm1 = new DidIidDriver().resolve(did, Map.of());
+        System.out.println(rdm1);
+    }
+
     public DidIidDriver() {
     }
 
     @Override
-    public ResolveResult resolve(String identifier) throws ResolutionException {
+    public ResolveDataModelResult resolve(DID did, Map<String, Object> resolveOptions) throws ResolutionException {
+        log.info("进入iid解析方法");
+        String didString = did.getDidString();
         // match
-        Matcher matcher = DID_IID_PATTERN.matcher(identifier);
+        Matcher matcher = DID_IID_PATTERN.matcher(didString);
         if (!matcher.matches()) {
             return null;
         }
         // fetch data from iid_service
-        String resolveUrl = this.getIIDUrl() + "/did/v1/get/" + identifier;
+        String resolveUrl = this.getIIDUrl() + "/iidservice/did/v1/get/" + Base64.encode(didString.getBytes(StandardCharsets.UTF_8));
         HttpGet httpGet = new HttpGet(resolveUrl);
 
         // find the DDO
@@ -63,67 +78,78 @@ public class DidIidDriver implements Driver {
         try {
             CloseableHttpResponse httpResponse = (CloseableHttpResponse) this.getHttpClient().execute(httpGet);
             if (httpResponse.getStatusLine().getStatusCode() != 200) {
-                throw new ResolutionException("Cannot retrieve DDO for `" + identifier + "` from `" + this.getIIDUrl() + ": " + httpResponse.getStatusLine());
+                log.info("iid解析失败");
+                throw new ResolutionException("Cannot retrieve DDO for `" + did + "` from `" + this.getIIDUrl() + ": " + httpResponse.getStatusLine());
             }
-
             // extract payload
             HttpEntity httpEntity = httpResponse.getEntity();
             String entityString = EntityUtils.toString(httpEntity);
             EntityUtils.consume(httpEntity);
-
-
             //  get DDO
             JSONObject jo = new JSONObject(entityString);
-            didDocumentDO = jo.getJSONObject("content").getJSONObject("didDocument");
+            didDocumentDO = jo.getJSONObject("data").getJSONObject("didDocument");
 
         } catch (IOException ex) {
-            throw new ResolutionException("Cannot retrieve DDO info for `" + identifier + "` from `" + this.getIIDUrl() + "`: " + ex.getMessage(), ex);
+            log.info("iid解析失败" + ex.getMessage());
+            throw new ResolutionException("Cannot retrieve DDO info for `" + did + "` from `" + this.getIIDUrl() + "`: " + ex.getMessage(), ex);
         } catch (JSONException jex) {
+            log.info("iid解析失败" + jex.getMessage());
             throw new ResolutionException("Cannot parse JSON response from `" + this.getIIDUrl() + "`: " + jex.getMessage(), jex);
         }
-
-        // DDO id
-        String id = identifier;
-
+        // context
+        List<String> context = new ArrayList<>();
+        context.add(didDocumentDO.getString("@context"));
+        List<URI> contexts = context.stream()
+                // the following context is added by default
+                .filter(con -> !"https://w3id.org/did/v1".equals(con))
+                .map(URI::create)
+                .collect(Collectors.toList());
         // DDO publicKeys
-        List<PublicKey> publicKeys = new ArrayList<>();
         // index 0 is auth key
         JSONObject firstKeyJO = didDocumentDO.getJSONArray("publicKey").getJSONObject(0);
-        publicKeys.add(PublicKey.build(
-                firstKeyJO.getString("id"),
-                DIDDOCUMENT_PUBLICKEY_TYPES,
-                null,
-                null,
-                firstKeyJO.getString("publicKeyHex"),
-                null));
         // index 1 is recovery key
         JSONObject secondKeyJO = didDocumentDO.getJSONArray("publicKey").getJSONObject(1);
-        publicKeys.add(PublicKey.build(
-                secondKeyJO.getString("id"),
-                DIDDOCUMENT_PUBLICKEY_TYPES,
-                null,
-                null,
-                secondKeyJO.getString("publicKeyHex"),
-                null));
-
-        // DDO Authentications
-        List<Authentication> authentications = new ArrayList<>();
-        authentications.add(Authentication.build(
-                null,
-                DIDDOCUMENT_AUTHENTICATION_TYPES,
-                firstKeyJO.getString("id")));
-
-        // DDO services
-        List<Service> services = new ArrayList<>();
+        //verificationMethod
+        List<Map<String, Object>> verificationMethod = new ArrayList<>();
+        Map<String, Object> verificationMethodMap = new HashMap<>();
+        verificationMethodMap.put("verificationMethod1", VerificationMethod.builder()
+                .controller(firstKeyJO.getString("id"))
+                .publicKeyHex(firstKeyJO.getString("publicKeyHex"))
+                .build());
+        Map<String, Object> verificationMethodMap2 = new HashMap<>();
+        verificationMethodMap2.put("verificationMethod2", VerificationMethod.builder()
+                .controller(secondKeyJO.getString("id"))
+                .publicKeyHex(secondKeyJO.getString("publicKeyHex"))
+                .build());
+        verificationMethod.add(verificationMethodMap);
+        verificationMethod.add(verificationMethodMap2);
+        //authentication
+        List<Map<String, Object>> authentication = new ArrayList<>();
+        Map<String, Object> authenticationMap = new HashMap<>();
+        authenticationMap.put("authentication", VerificationMethod.builder()
+                .controller(firstKeyJO.getString("id"))
+                .build());
+        authentication.add(authenticationMap);
+        //services
+        List<Map<String, Object>> services = new ArrayList<>();
         JSONArray serviceJA = didDocumentDO.getJSONArray("service");
         for (int i = 0; i < serviceJA.length(); i++) {
             JSONObject service = serviceJA.getJSONObject(i);
-            services.add(Service.build(service.getString("type"), service.getString("serviceEndpoint")));
+            Map<String, Object> serviceMap = new HashMap<>();
+            serviceMap.put("service", Service.builder()
+                    .type(service.getString("type"))
+                    .serviceEndpoint(service.getString("serviceEndpoint"))
+                    .build());
+            services.add(serviceMap);
         }
-
         // create DDO
-        DIDDocument didDocument = DIDDocument.build(id, publicKeys, authentications, services);
-
+        DIDDocument didDocument = DIDDocument.builder()
+                .contexts(contexts)
+                .id(did.toUri())
+                .verificationMethods(intoVerificationMethods(verificationMethod))
+                .authenticationVerificationMethods(intoVerificationMethods(authentication))
+                .services(intoServices(services))
+                .build();
 
         // create Method METADATA
         Map<String, Object> methodMetadata = new LinkedHashMap<>();
@@ -132,12 +158,23 @@ public class DidIidDriver implements Driver {
         methodMetadata.put("created", didDocumentDO.getString("created"));
         methodMetadata.put("updated", didDocumentDO.getString("updated"));
 
-
-        // create RESOLVE RESULT
-        ResolveResult resolveResult = ResolveResult.build(didDocument, null, methodMetadata);
-
         // done
-        return resolveResult;
+        log.info("iid解析结束");
+        return ResolveDataModelResult.build(null, didDocument, methodMetadata);
+    }
+
+    private static List<VerificationMethod> intoVerificationMethods(List<Map<String, Object>> list) {
+        return intoBeans(list, VerificationMethod::fromMap);
+    }
+
+    private static List<Service> intoServices(List<Map<String, Object>> list) {
+        return intoBeans(list, Service::fromMap);
+    }
+
+    private static <T> List<T> intoBeans(List<Map<String, Object>> list, Function<Map<String, Object>, T> mapper) {
+        return list.stream()
+                .map(mapper)
+                .collect(Collectors.toList());
     }
 
     @Override
